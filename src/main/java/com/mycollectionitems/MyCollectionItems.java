947a290b6852a006a18f3a1a7b1ca63ec5feb137
LucasSangino <Lucas.Sangino@alu.frt.utn.edu.ar>
2025-12-27 12:22:09 -0300

    establecer la base del proyecot

---
+++ src/main/java/com/mycollectionitems/MyCollectionItems.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycollectionitems;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class MyCollectionItems {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
